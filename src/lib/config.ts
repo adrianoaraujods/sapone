f1d542bfd2c0e37be498d8d5966734b535bd7890
Adriano Araújo <adrianoaraujodsantos@hotmail.com>
2025-06-17 20:55:25 -0300

    refactor: remove unused configuration

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,10 +2,3 @@ export const THEMES = ["system", "light", "dark"] as const;
 export type Theme = (typeof THEMES)[number];
 
 export const GITHUB_URL = `https://github.com/adrianoaraujods/sapone`;
-
-export type NavigationLink = {
-  title: string;
-  href: string;
-};
-
-export const NAVIGATION_LINKS: NavigationLink[] = [];
